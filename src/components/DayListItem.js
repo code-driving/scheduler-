777b50e23d523a1f67f1e141c4e260fdddebf4f0
Tetyana Titovets <tittan7@gmail.com>
2021-04-12 17:54:18 -0400

    Complete Button and DayListItem components

---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+const classNames = require("classnames");
+
+export default function DayListItem(props) {
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return `no spots remaining`;
+    }
+    if (spots === 1) {
+      return `1 spot remaining`;
+    } else {
+      return `${spots} spots remaining`;
+    }
+  }
+  
+  
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}

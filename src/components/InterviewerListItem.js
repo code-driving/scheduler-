3b003dc7d83b35c7de155f6f026212efa3cc791c
Tetyana Titovets <tittan7@gmail.com>
2021-04-12 23:47:33 -0400

    Add correct class to the image in InterviewerListItem component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,13 +5,15 @@ const classNames = require("classnames");
 
 export default function InterviewerListItem (props) {
   let interviewersClass = classNames("interviewers__item", {
-    "interviewers__item-image": props.avatar,
     "interviewers__item--selected": props.selected
   });
+  let imageClass = classNames("interviewers__item-image", {
+    "interviewers__item--selected-image": props.selected
+  })
   return (
     <li className={interviewersClass} onClick={() => props.setInterviewer(props.name)}>
     <img
-      className={interviewersClass}
+      className={imageClass}
       src={props.avatar}
       alt="Interviewers name"
     />

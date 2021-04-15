0de0ef33cfc37cb7d3c709e38d2fbc1653f791cb
Tetyana Titovets <tittan7@gmail.com>
2021-04-14 20:17:34 -0400

    Retrieve Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,88 +4,102 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
+import getAppointmentsForDay from "helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "3pm",
-  },
-  {
-    id: 4,
-    time: "2pm",
-    interview: {
-      student: "James Doe",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      },
-    },
-  },
-  {
-    id: 5,
-    time: "1pm",
-  },
-  {
-    id: 6,
-    time: "12pm",
-    interview: {
-      student: "Sarah Wilberg",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      },
-    },
-  },
-  {
-    id: 7,
-    time: "3pm",
-  },
-  {
-    id: 8,
-    time: "5pm",
-    interview: {
-      student: "Jake Miller",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
+//   {
+//     id: 3,
+//     time: "3pm",
+//   },
+//   {
+//     id: 4,
+//     time: "2pm",
+//     interview: {
+//       student: "James Doe",
+//       interviewer: {
+//         id: 2,
+//         name: "Tori Malcolm",
+//         avatar: "https://i.imgur.com/Nmx0Qxo.png",
+//       },
+//     },
+//   },
+//   {
+//     id: 5,
+//     time: "1pm",
+//   },
+//   {
+//     id: 6,
+//     time: "12pm",
+//     interview: {
+//       student: "Sarah Wilberg",
+//       interviewer: {
+//         id: 3,
+//         name: "Mildred Nazir",
+//         avatar: "https://i.imgur.com/T2WwVfS.png",
+//       },
+//     },
+//   },
+//   {
+//     id: 7,
+//     time: "3pm",
+//   },
+//   {
+//     id: 8,
+//     time: "5pm",
+//     interview: {
+//       student: "Jake Miller",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       },
+//     },
+//   },
+// ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
-  
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url: `/api/days`,
-    }).then((response) => {
-      setDays(response.data);
-    });
-  }, []);
 
-  const list = appointments.map((appointment) => {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  });
+  
+  const setDay = (day) => setState({ ...state, day });
+  
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("api/days"), 
+      axios.get("api/appointments")])
+    .then((all) => {
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+        }));
+      }
+    );
+  });
+
+  const list = dailyAppointments.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
 
@@ -100,7 +114,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"

e883ba83516496d2dd127b896f1aaa4d5ccba924
Tetyana Titovets <tittan7@gmail.com>
2021-04-17 16:38:54 -0400

    Separation of Concerns

--- src/components/Application.js
+++ src/components/Application.js
@@ -16,13 +16,13 @@ export default function Application(props) {
     state,
     setDay,
     bookInterview,
-    cancelInterview
+    cancelInterview,
   } = useApplicationData();
 
   const interviewers = getInterviewersForDay(state, state.day);
 
   const appointments = getAppointmentsForDay(state, state.day).map(
-    appointment => {
+    (appointment) => {
       return (
         <Appointment
           key={appointment.id}
@@ -63,111 +63,3 @@ export default function Application(props) {
     </main>
   );
 }
-
-// export default function Application(props) {
-//   const [state, setState] = useState({
-//     day: "Monday",
-//     days: [],
-//     appointments: {},
-//     interviewers: {},
-//   });
-
-//   const setDay = (day) => setState({ ...state, day });
-
-//   useEffect(() => {
-//     Promise.all([
-//       axios.get("/api/days"),
-//       axios.get("/api/appointments"),
-//       axios.get("/api/interviewers"),
-//     ]).then((all) => {
-//       setState((prev) => ({
-//         ...prev,
-//         days: all[0].data,
-//         appointments: all[1].data,
-//         interviewers: all[2].data,
-//       }));
-//     });
-//   }, []);
-
-//   const dailyAppointments = getAppointmentsForDay(state, state.day);
-
-//   const schedule = dailyAppointments.map((appointment) => {
-//     const interview = getInterview(state, appointment.interview);
-//     const interviewers = getInterviewersForDay(state, state.day);
-//     return (
-//       <Appointment
-//         key={appointment.id}
-//         id={appointment.id}
-//         time={appointment.time}
-//         interview={interview}
-//         interviewers={interviewers}
-//         bookInterview={bookInterview}
-//         cancelInterview={cancelInterview}
-//       />
-//     );
-//   });
-
-//   function bookInterview(id, interview) {
-//     const appointment = {
-//       ...state.appointments[id],
-//       interview: { ...interview },
-//     };
-//     const appointments = {
-//       ...state.appointments,
-//       [id]: appointment,
-//     };
-//     return axios
-//       .put(`/api/appointments/${id}`, { interview })
-//       .then((response) => {
-//         setState({
-//           ...state,
-//           appointments,
-//         });
-//       });
-//   }
-
-//   function cancelInterview(id) {
-//     const appointment = {
-//       ...state.appointments[id],
-//       interview: null,
-//     };
-//     const appointments = {
-//       ...state.appointments,
-//       [id]: appointment,
-//     };
-
-//     return axios.delete(`/api/appointments/${id}`).then((response) => {
-//       setState({
-//         ...state,
-//         appointments,
-//       });
-//     });
-//   }
-
-//   return (
-//     <main className="layout">
-//       <section className="sidebar">
-//         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
-//         <img
-//           className="sidebar--centered"
-//           src="images/logo.png"
-//           alt="Interview Scheduler"
-//         />
-//         <hr className="sidebar__separator sidebar--centered" />
-//         <nav className="sidebar__menu">
-//           <DayList days={state.days} day={state.day} setDay={setDay} />
-//         </nav>
-//         <img
-//           className="sidebar__lhl sidebar--centered"
-//           src="images/lhl.png"
-//           alt="Lighthouse Labs"
-//         />
-//       </section>
-//       <section className="schedule">
-//         {schedule}
-//         <Appointment key="last" time="5pm" />
-//         {/* <Appointment key="last" time="5pm" bookInterview={bookInterview} cancelInterview={cancelInterview}/> */}
-//       </section>
-//     </main>
-//   );
-// }
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -62,6 +62,7 @@ export default function useApplicationData(props) {
       });
     });
   }
+
   return {
     state,
     setDay,

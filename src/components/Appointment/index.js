fcc1f74f2a614b86b460c00148ce54892e1b8a46
Tetyana Titovets <tittan7@gmail.com>
2021-04-23 14:16:14 -0400

    Add condition while making an appointment without interviewer name

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -39,13 +39,15 @@ export default function Appointment(props) {
       student: name,
       interviewer,
     };
-
-    transition(SAVING);
-
-    props
-      .bookInterview(props.id, interview)
-      .then(() => transition(SHOW))
-      .catch((error) => transition(ERROR_SAVE, true));
+    if (name && interviewer) {
+      transition(SAVING);
+      props
+        .bookInterview(props.id, interview)
+        .then(() => transition(SHOW))
+        .catch((error) => transition(ERROR_SAVE, true));
+    } else {
+      transition(ERROR_SAVE, true);
+    }
   }
 
   function destroy(event) {
